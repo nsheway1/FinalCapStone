64a173ed6e1185e09fe13b2a6646f50785dfc69c
Dave Radosevic <dradosevic1@gmail.com>
2021-12-04 12:20:32 -0500

    JSON for brewery list displayin in browser. Going to style and add new backend

---
+++ vue/src/components/BreweryList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <div v-for="brewery in breweries" v-bind:key="brewery.id">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+import breweryService from '../services/BreweryService.js';
+
+export default {
+    name: 'brewery-list',
+    data() {
+        return {
+            breweries: []
+        }
+    },
+    created() {
+        breweryService.getBreweries().then( response => {
+            this.breweries = response.data;
+        })
+    }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ vue/src/views/AllBreweries.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <brewery-list />
+  </div>
+</template>
+
+<script>
+import BreweryList from '../components/BreweryList.vue'
+export default {
+    name: 'all-breweries',
+  components: { BreweryList }
+
+}
+</script>
+
+<style>
+
+</style>
