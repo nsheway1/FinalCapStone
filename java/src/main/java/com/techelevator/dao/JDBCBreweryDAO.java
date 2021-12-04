7f16d029a9106fd9c9ed4d78228e5cf1aac36aca
Dave Radosevic <dradosevic1@gmail.com>
2021-12-04 11:20:24 -0500

    brewery DAO getAllBreweries method working, test passing, need to write controller

--- java/src/main/java/com/techelevator/dao/JDBCBreweryDAO.java
+++ java/src/main/java/com/techelevator/dao/JDBCBreweryDAO.java
@@ -2,6 +2,7 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Brewery;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +16,27 @@ public class JDBCBreweryDAO implements BreweryDAO{
     }
 
     public List<Brewery> getAllBreweries() {
-        List<Brewery> breweries = new ArrayList<>();
+        List<Brewery> breweries = new ArrayList<Brewery>();
+        String sql = "SELECT id, name, description, street_address, city, state, zipcode FROM brewery";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            Brewery brewery = mapRowToBrewery(results);
+            breweries.add(brewery);
+        }
+
         return breweries;
+    }
+
+    private Brewery mapRowToBrewery(SqlRowSet results) {
+        Brewery brewery = new Brewery();
+        brewery.setId(results.getLong("id"));
+        brewery.setName(results.getString("name"));
+        brewery.setDescription(results.getString("description"));
+        brewery.setStreetAddress(results.getString("street_address"));
+        brewery.setCity(results.getString("city"));
+        brewery.setState(results.getString("state"));
+        brewery.setZipcode(results.getString("zipcode"));
+
+        return brewery;
     }
 }
