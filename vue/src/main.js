df24f63e2b4f9f59609ba8e382479e5c78990e1e
Dave Radosevic <dradosevic1@gmail.com>
2021-12-11 13:57:44 -0500

    hoping we have fixed all of our problems

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,20 +5,6 @@ import store from './store/index'
 import axios from 'axios'
 import * as VueGoogleMaps from 'vue2-google-maps'
 import Geocoder from "@pderas/vue2-geocoder"
-import firebase from 'firebase'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC5HGZSVCEh5xfUfNYs7pqU4JKYGTf8E4Y",
-  authDomain: "brewery-finder-f943e.firebaseapp.com",
-  projectId: "brewery-finder-f943e",
-  storageBucket: "brewery-finder-f943e.appspot.com",
-  messagingSenderId: "107821408111",
-  appId: "1:107821408111:web:313f745afed0be3c44129a",
-  measurementId: "G-EF9MZWVRK7"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
