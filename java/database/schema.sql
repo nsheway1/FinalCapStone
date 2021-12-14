9f8a4628b021cd58accdf569f388208a36e52f7d
Paul Lloyd <plloyd93@gmail.com>
2021-12-14 15:11:07 -0500

    saved schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -30,7 +30,9 @@ CREATE TABLE brewery(
         street_address varchar(200) not null,
         city varchar(200) not null,
         state varchar(200) not null,
-        zipcode varchar(15) not null
+        zipcode varchar(15) not null,
+        vote_count int,
+        carousel_count int
 );
 
 
@@ -46,13 +48,13 @@ CREATE TABLE beer(
 );
 
 INSERT INTO brewery VALUES (default, 'Seventh Son', 'Seventh Son Brewing has been producing exceptional beers at the intersection of 4th & 4th in Columbus’ historic Italian Village since April of 2013.',
-'1101 N. 4th St.', 'Columbus', 'Ohio', '43201');
+'1101 N. 4th St.', 'Columbus', 'Ohio', '43201', 0, 0);
 INSERT INTO brewery VALUES (default, 'Hoof-Hearted', 'Here at Hoof Hearted Brewing we have been turning tanks like cassette tapes since late 2011. Known throughout the industry for our deft hop-wrangling and nude air guitar prowess, we do everything we can to provide fresh, high-quality (and sometimes hi-test) liquid to help you get the party poppin',
-'850 N 4th St.', 'Columbus', 'Ohio', '43215');
-INSERT INTO brewery VALUES (default, 'Edison', 'Fresh brews that will leave you needing more', '785 Science Blvd.', 'Gahanna', 'Ohio', '43230');
-INSERT INTO brewery VALUES (default, 'Nostalgia', 'Beer that starts a conversation is the essence of Nostalgia Brewing', '81 Mill St', 'Gahanna', 'Ohio', '43230');
-INSERT INTO brewery VALUES (default, 'North High', 'Beer so good they named a street after us', '1288 N High St', 'Columbus', 'Ohio', '43201');
-INSERT INTO brewery VALUES (default, 'Endeavor', 'There is a story behind every beer', '909 W 5th Ave', 'Columbus', 'Ohio', '43212');
+'850 N 4th St.', 'Columbus', 'Ohio', '43215', 0, 0);
+INSERT INTO brewery VALUES (default, 'Edison', 'Fresh brews that will leave you needing more', '785 Science Blvd.', 'Gahanna', 'Ohio', '43230', 0, 0);
+INSERT INTO brewery VALUES (default, 'Nostalgia', 'Beer that starts a conversation is the essence of Nostalgia Brewing', '81 Mill St', 'Gahanna', 'Ohio', '43230', 1, 0);
+INSERT INTO brewery VALUES (default, 'North High', 'Beer so good they named a street after us', '1288 N High St', 'Columbus', 'Ohio', '43201', 0, 0);
+INSERT INTO brewery VALUES (default, 'Endeavor', 'There is a story behind every beer', '909 W 5th Ave', 'Columbus', 'Ohio', '43212', 0, 0);
 
 INSERT INTO beer VALUES (default, 'Humulus Nimbus', 'Pale Ale', 'A pale golden ale that is both super crisp and super hop forward with a refreshing mouthfeel and a summer friendly 6% abv. Mosaic & simcoe hops lend tart blueberry and fragrant pine to a pleasingly bitter dandelion finish.',
 6.0, 1);
@@ -88,5 +90,3 @@ INSERT INTO beer VALUES (default, 'New World', 'IPA', 'Hops from three continent
 INSERT INTO beer VALUES (default, 'Shaka', 'IPA', 'Select hops from three separate continents provide an accentuated aroma of tropical fruit, while keeping bitterness, alcohol content, and malt base balanced.', 6.5, 6);
 
 COMMIT TRANSACTION;
-
-
